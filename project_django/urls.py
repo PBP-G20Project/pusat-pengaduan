c2ce81bb943b4f779752fa3c15ee330e4ad9888a
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2022-10-28 23:41:26 +0700

    add url submission_form

--- project_django/urls.py
+++ project_django/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
+    path('submission_form/', include('submission_form.urls'), name='submission_form'),
 ]
