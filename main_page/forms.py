80312c977a7579b9b1d471f2bfd3bc0c18195fd7
kennethferdinandd <kennethferdinand03@gmail.com>
2022-10-29 23:53:45 +0700

    update

--- main_page/forms.py
+++ main_page/forms.py
@@ -3,14 +3,15 @@ from django.forms import ModelForm
 from django import forms
 from .models import *
 
+
 class FormReviews(ModelForm):
     class Meta:
         model = Reviews
         fields = ['rating', 'comment']
 
         widgets = {
-            'rating' : forms.NumberInput({'class':'form-control'}),   
-            'comment' : forms.Textarea({'class':'form-control'}),    
+            'rating': forms.NumberInput({'class': 'form-control'}),
+            'comment': forms.TextInput({'class': 'form-control'}),
         }
 
 # class PostForms(forms.ModelForm):
