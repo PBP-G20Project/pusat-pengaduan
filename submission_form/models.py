d94dc2017b1e6eefd6451e6fdb88dc0583321001
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2022-10-30 16:14:01 +0700

    add institution_level

--- submission_form/models.py
+++ submission_form/models.py
@@ -3,6 +3,14 @@ from django.db import models
 # Create your models here.
 
 # Report: Judul, Isi Laporan, Tanggal, Lokasi, Instansi, Pihak yang terlibat, Status
+
+INSTITUTION_LEVEL = (
+    ('Nasional', 'Nasional'),
+    ('Provinsi', 'Provinsi'),
+    ('Kabupaten/Kota', 'Kabupaten/Kota'),
+    ('Kecamatan', 'Kecamatan'),
+    ('Desa/Kelurahan', 'Desa/Kelurahan'),
+)
 class Report(models.Model):
     user = models.CharField(max_length=100) # pake object user
     admin = models.CharField(max_length=100) # pake object admin
@@ -10,6 +18,7 @@ class Report(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     institution = models.CharField(max_length=100)
+    institution_level = models.CharField(max_length=100, choices=INSTITUTION_LEVEL)
     involved_party = models.CharField(max_length=100)
     date = models.DateTimeField()
     location = models.CharField(max_length=100)
