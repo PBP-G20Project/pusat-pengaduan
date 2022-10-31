321360e363989328a4ccc34a2f9361f4431d1931
rubentanoey <92169008+rubentanoey@users.noreply.github.com>
2022-10-31 22:48:29 +0700

    Logic Dashboard Admin

--- dashboard_admin/views.py
+++ dashboard_admin/views.py
@@ -7,23 +7,20 @@ from django.http import HttpResponseRedirect, HttpResponse
 # Create your views here.
 #@login_required(login_url = '/login/')
 def show_report(request):
-    report_objects = Report.objects.filter(user=request.user_submission_id)
+    report_objects = Report.objects.filter(admin_submission = request.user)
     context = {
         "report": report_objects,
         "username": request.user}
     return render(request, "accusation.html", context)
 
-#@login_required(login_url = "/login/")
-def update_diproses (request, id):
-    report = Report.objects.get(user = request.user, id=id)
-    report.diproses = not (report.diproses)
-    report.save(update_fields = ["diproses"])
-    return HttpResponseRedirect(reverse("dashboard_admin:show_report"))
+def report_next(request):
+    report_objects = Report.objects.filter(admin_submission = request.user, id=id)
+    update_status_next(report_objects)
 
-#login_required(login_url = "/login/")
-def update_selesai (request, id):
-    report = Report.objects.get(user = request.user, id=id)
-    report.selesai = not (report.selesai)
-    report.save(update_fields = ["selesai"])
-    return HttpResponseRedirect(reverse("dashboard_admin:show_report"))
+def report_prev(request):
+    report_objects = Report.objects.filter(admin_submission = request.user, id=id)
+    update_status_back(report_objects)
 
+def report_reject(request):
+    report_objects = Report.objects.filter(admin_submission = request.user, id=id)
+    update_status_reject(report_objects)
