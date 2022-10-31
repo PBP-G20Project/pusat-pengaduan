105bd0024d0123edd66a9028b2db23574b4e565d
Nur Fajar Sayyidul Ayyam <nur.fajar11@ui.ac.id>
2022-10-31 15:58:30 +0700

    update view

--- submission_form/views.py
+++ submission_form/views.py
@@ -1,15 +1,14 @@
 from submission_form.forms import ReportForm
 from submission_form.models import Report
 from login_things.models import User
-import random
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-from django.contrib import messages
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse
 from django.core import serializers
+import random
 
 # Create your views here.
-@csrf_exempt
+# @csrf_exempt
 def show_form(request):
     context = {}
     return render(request, 'form.html', context)
@@ -19,7 +18,7 @@ def get_json(request):
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 
-@csrf_exempt
+# @csrf_exempt
 def create_report(request):
     data_admin = User.objects.filter(admin=True).filter(staff=False)
     index = random.randint(0, len(data_admin)-1)
@@ -31,7 +30,6 @@ def create_report(request):
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            
             report = Report(
                 user_submission = request.user,
                 admin_submission = data_admin[index],
@@ -51,12 +49,9 @@ def create_report(request):
                 serializers.serialize("json", [report]),
                 content_type="application/json",
             )
-        else:
-            print("Isian kosong")
 
     # if a GET (or any other method) we'll create a blank form
     else:
         form = ReportForm()
-        print("error2")
 
     return render(request, 'form.html', {'form': form})
