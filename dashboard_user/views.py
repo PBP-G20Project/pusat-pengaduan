5adb983f8c35d7bca93c08c543449ee03a4f7356
raspatimahatma <tommymahatma08@gmail.com>
2022-11-02 12:04:16 +0700

    update views and html

--- dashboard_user/views.py
+++ dashboard_user/views.py
@@ -7,24 +7,34 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url = '/login/')
 def show_reports(request):
-    return render(request,"user-dashboard.html")
+    data = Report.objects.filter(user_submission = request.user)
+    context = {
+        "report" : data,
+        "username":request.user,
+    }
+    return render(request,"user-dashboard.html",context)
 
 @login_required(login_url='/login/')
 def get_all_reports(request):
-    data = Report.objects.filter(user = request.user)
+    data = Report.objects.filter(user_submission = request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 @login_required(login_url = '/login/')
 def get_unprocessed_reports(request):
-    data = Report.objects.filter(user=request, status ="Belum")
+    data = Report.objects.filter(user_submission=request.user, status ="PENDING")
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 @login_required(login_url = '/login/')
 def get_onprogress_reports(request):
-    data = Report.objects.filter(user = request.user ,status ='Sedang')
+    data = Report.objects.filter(user_submission = request.user ,status ='DIPROSES')
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 @login_required(login_url = '/login/')
 def get_processed_reports(request):
-    data = Report.objects.filter(user=request.user, status='Sudah')
+    data = Report.objects.filter(user_submission=request.user, status='SELESAI')
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+@login_required(login_url = '/login/')
+def get_rejected_reports(request):
+    data = Report.objects.filter(user_submission=request.user, status='REJECTED')
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
