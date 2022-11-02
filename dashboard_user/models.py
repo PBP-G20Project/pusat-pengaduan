528c4b2728803e991b0c1d1d4f054efa6998d5e8
raspatimahatma <tommymahatma08@gmail.com>
2022-11-02 16:38:53 +0700

    create forms and update views and html

--- dashboard_user/models.py
+++ dashboard_user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from login_things.models import User
 # Create your models here.
+
+class Draft(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    date = models.DateTimeField(auto_now=True)
+    title = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
--- dashboard_user/views.py
+++ dashboard_user/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core import serializers
 from submission_form.models import Report
+from dashboard_user.models import Draft
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 @login_required(login_url = '/login/')
 def show_reports(request):
@@ -15,31 +18,45 @@ def show_reports(request):
 def get_all_reports(request):
     if request.user.admin and not request.user.staff:
         return redirect("login:error_page")
-    data = Report.objects.filter(user = request.user)
+    data = Report.objects.filter(user_submission = request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 @login_required(login_url = '/login/')
 def get_unprocessed_reports(request):
     if request.user.admin and not request.user.staff:
         return redirect("login:error_page")
-    data = Report.objects.filter(user=request.user, status ="PENDING")
+    data = Report.objects.filter(user_submission=request.user, status ="PENDING")
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 @login_required(login_url = '/login/')
 def get_onprogress_reports(request):
     if request.user.admin and not request.user.staff:
         return redirect("login:error_page")
-    data = Report.objects.filter(user = request.user ,status ='DIPROSES')
+    data = Report.objects.filter(user_submission = request.user ,status ='DIPROSES')
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 @login_required(login_url = '/login/')
 def get_processed_reports(request):
     if request.user.admin and not request.user.staff:
         return redirect("login:error_page")
-    data = Report.objects.filter(user=request.user, status='SELESAI')
+    data = Report.objects.filter(user_submission =request.user, status='SELESAI')
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 @login_required(login_url = '/login/')
 def get_rejected_reports(request):
     data = Report.objects.filter(user_submission=request.user, status='REJECTED')
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+@login_required(login_url = '/login/')
+@csrf_exempt
+def add_draft(request):
+    context = {}
+    if request.method == "POST":
+        temp = Draft(user=request.user, title=request.POST.get('title'), description=request.POST.get('description'))
+        temp.save()
+        return JsonResponse({'message': 'success'})
+
+@login_required(login_url = '/login/')
+def show_draft_json(request):
+    data = Draft.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
