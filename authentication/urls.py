b5f8072ee9f61579642cca09d19b170fa62e8e3f
Abdul Rafi <abdulrafi907@gmail.com>
2022-12-10 12:15:11 +0700

    tambah data login

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,4 +6,5 @@ app_name = 'login_baru'
 
 urlpatterns = [
     path('login/', login, name='login'),
+    path('data_login/', data_login, name='data_login'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login
-from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from login_things.models import User
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
+
 @csrf_exempt
 def login(request):
     email = request.POST.get('email')
     password = request.POST.get('password')
-    user = authenticate(request, username=email, password=password)
+    user = authenticate(username=email, password=password)
     if user is not None:
         if user.is_active:
             auth_login(request, user)
@@ -30,19 +31,7 @@ def login(request):
           "message": "Failed to Login, check your email/password."
         }, status=401)
 
-# def login_user(request):
-#     if request.method == 'POST':
-#         email = request.POST.get('email')
-#         password = request.POST.get('password')
-#         user = authenticate(request, username=email, password=password)
-#         if user is not None:
-#             login(request, user)
-#             response = HttpResponseRedirect(
-#                 reverse("main_page:show_main_page"))  # membuat response
-#             # membuat cookie last_login dan menambahkannya ke dalam response
-#             response.set_cookie('last_login', str(datetime.datetime.now()))
-#             return response
-#         else:
-#             messages.error(request, 'Email atau Password salah!')
-#     context = {}
-#     return render(request, 'login.html', context)
+@login_required(login_url='/login/')
+def data_login(request):
+  user_data = User.objects.filter(id=request.user.id)
+  return HttpResponse(serializers.serialize('json', user_data), content_type="application/json")
