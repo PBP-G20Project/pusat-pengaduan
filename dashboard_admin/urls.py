27946b70e0329fb7249efdfc96991f35d887fe93
rubentanoey <92169008+rubentanoey@users.noreply.github.com>
2022-12-12 21:40:34 +0700

    Try

--- dashboard_admin/urls.py
+++ dashboard_admin/urls.py
@@ -13,6 +13,7 @@ from dashboard_admin.views import show_all_ditolak
 from dashboard_admin.views import show_reminder_form
 from dashboard_admin.views import get_reminder_json
 from dashboard_admin.views import add_reminder_json
+from dashboard_admin.views import create_reminder
 
 app_name = 'dashboard_admin'
 
@@ -23,6 +24,7 @@ urlpatterns = [
     path('report_reject/<int:id>/', report_reject, name='report_reject'),
     path('show_reminder_form/', show_reminder_form, name='show_reminder_form'),
     path('create_note/', create_note, name='create_note'),
+    path('create_reminder/', create_reminder, name='create_reminder'),
     path('add_reminder_json/', add_reminder_json, name='add_reminder_json'),
     path('get_reminder_json/', get_reminder_json, name='get_reminder_json'),
     path('show_all_report/', show_all_report, name='show_all_report'),
